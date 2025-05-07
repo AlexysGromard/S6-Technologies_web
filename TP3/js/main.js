1ad73ea510a84307a35adf1f99132ad8cd9f3725
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-07 16:56:52 +0200

    Refactor loadGenre function to use async/await for better readability and error handling

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -4,41 +4,37 @@
 // Pas besoin d'évenement window.onload puisqu'on utilise l'attribut defer
 // lorsque l'on charge notre script
 
-function loadGenre() {
+async function loadGenre() {
     const genreUrl = 'http://localhost:3000/genres';
+    try {
+        const response = await fetch(genreUrl);
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        const data = await response.json();
 
-    fetch(genreUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Erreur HTTP : ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Data :', data);
-            // Update the select element with the genres
-            let selectGenre = document.getElementById('select-genre');
-            if (!selectGenre) {
-                console.error('Select element not found');
-                return;
-            }
+        // Update the select element with the genres
+        let selectGenre = document.getElementById('select-genre');
+        if (!selectGenre) {
+            console.error('Select element not found');
+            return;
+        }
 
-            data.forEach(genre => {
-                const option = document.createElement('option');
-                option.value = genre.id;
-                option.textContent = genre.name;
-                selectGenre.appendChild(option);
-            });
-            // Add event listener for genre change
-            selectGenre.addEventListener('change', (event) => {
-                console.log('Selected genre:', event.target.value);
-            });
-        })
-        .catch(error => {
-            console.error(error.message);
+        data.forEach(genre => {
+            const option = document.createElement('option');
+            option.value = genre.id;
+            option.textContent = genre.name;
+            selectGenre.appendChild(option);
         });
-}
 
+        // Add event listener for genre change
+        selectGenre.addEventListener('change', (event) => {
+            console.log('Selected genre:', event.target.value);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
 loadGenre()
 
