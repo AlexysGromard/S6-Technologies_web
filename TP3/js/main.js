5dc9b596ec5481f17a03ed5f33e16b6d7aa49426
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-07 16:36:18 +0200

    Ajout du script principal et de la fonction de chargement des genres

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -3,3 +3,25 @@
 
 // Pas besoin d'évenement window.onload puisqu'on utilise l'attribut defer
 // lorsque l'on charge notre script
+
+function loadGenre() {
+    const genreUrl = 'http://localhost:3000/genres';
+
+    fetch(genreUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP : ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data :', data);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des genres :', error.message);
+        });
+}
+
+loadGenre()
+
+console.log('JS file successfully loaded');
